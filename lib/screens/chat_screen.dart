3afc5b46172b1700b2146f24eab91c353a4c8c40
happy <hhongg@nate.com>
2023-09-15 06:23:08 +0900

    Init commit

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -33,23 +34,57 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Chat screen'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                _authentication.signOut();
-                Navigator.pop(context);
+        appBar: AppBar(
+          title: Text('Chat screen'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  _authentication.signOut();
+                  Navigator.pop(context);
+                },
+                icon: Icon(
+                  Icons.exit_to_app_sharp,
+                  color: Colors.white,
+                ))
+          ],
+        ),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('/chats/gmApqKy23goQDhDheIAw/message')
+              .snapshots(),
+          builder: (BuildContext context,
+              AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+
+            if(snapshot.connectionState==ConnectionState.waiting){
+              return Center(
+
+                child: CircularProgressIndicator(),
+
+
+
+              );
+            }
+
+            final docs = snapshot.data!.docs;
+            return ListView.builder(
+              itemCount: docs.length,
+              itemBuilder: (context,index){
+                return Container(
+                  padding: EdgeInsets.all(8.0) ,
+                  child: Text(
+                    docs[index]['text'],
+                    style: TextStyle(
+                      fontSize: 20.0
+                    ),
+
+                  ),
+
+
+
+                );
               },
-              icon: Icon(
-                Icons.exit_to_app_sharp,
-                color: Colors.white,
-              ))
-        ],
-      ),
-      body: Center(
-        child: Text('Chat screen'),
-      ),
-    );
+            );
+          },
+        ));
   }
 }
